3d7960e974447e8fd09242f40b02ea690969d5a9
noxtsu <94978861+noxtsu@users.noreply.github.com>
2022-08-16 00:35:26 +0900

    [create]main screen

--- app/src/main/java/com/example/jankenapp/MainActivity.kt
+++ app/src/main/java/com/example/jankenapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.jankenapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.example.jankenapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //手のボタンを推されたら呼ばれる
+        fun judgeJanken(view: View){
+            //どの手が押されたか
+            val judgeJanken: Button = findViewById(view.id)
+            val btnText = judgeJanken.text.toString()
+        }
+
+
     }
 }
