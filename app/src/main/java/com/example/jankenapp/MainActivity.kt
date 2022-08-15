b2471bba8fa37322d1bcaceed58547fdfad8bcc8
noxtsu <94978861+noxtsu@users.noreply.github.com>
2022-08-15 22:56:26 +0900

    first app

---
+++ app/src/main/java/com/example/jankenapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jankenapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
